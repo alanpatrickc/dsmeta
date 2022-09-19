94c7bf19fea706bfbeb3b40aec7b8c9929f99b61
alanpatrickc <alanpatrickc@hotmail.com>
2022-09-18 23:06:00 -0300

    useState

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,5 +1,5 @@
-import log from '../../assets/img/ds.svg'
-import './styles.css'
+import log from '../../assets/img/ds.svg';
+import './styles.css';
 function Header() {
     return(
         <header>
@@ -15,5 +15,5 @@ function Header() {
     )
    }
    
-   export default Header
+   export default Header;
    
